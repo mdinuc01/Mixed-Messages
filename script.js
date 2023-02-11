b0133b4d7f430e2a00a82f949abc8536fad59c40
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2023-02-10 23:45:35 -0500

    feat: created array that stores objects with joke information

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-
+const jokes = [
+  {
+    whoseThere: "Tank",
+    punchLine: "You're welcome."
+  },
+  {
+    whoseThere: "Leaf",
+    punchLine: "Leaf me alone!"
+  },
+  {
+    whoseThere: "Butter",
+    punchLine: "Butter let me in or I'll freeze!"
+  },
+  {
+    whoseThere: "Leash",
+    punchLine: "Lease you could do is answer the doorbell!"
+  },
+  {
+    whoseThere: "A broken pencil",
+    punchLine: "Never mind. It’s pointless."
+  },
+  {
+    whoseThere: "Leon",
+    punchLine: "Leon me ... when you're not strong!"
+  },
+  {
+    whoseThere: "Elly",
+    punchLine: "Elly-mentary, my dear Watson!"
+  },
+  {
+    whoseThere: "Euripides",
+    punchLine: "Euripides clothes, you pay for them!"
+  },
+  {
+    whoseThere: "A wood wok",
+    punchLine: "A wood wok 500 miles, and a wood wok 500 more!"
+  },
+  {
+    whoseThere: "Cargo",
+    punchLine: "Car go beep, beep!"
+  }
+];
